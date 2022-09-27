e11ab966b0efc47c136986736593e9594c685ab0
Ludwig Lord Johansson <ludwig.johansson@sr.se>
2022-09-27 13:49:52 +0000

    shot logic

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use invaders::{frame, render};
 use rusty_audio::Audio;
 use std::error::Error;
 use std::sync::mpsc;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::{io, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -65,8 +65,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Game Loop
     let mut player = Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop {
         // Per frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
         let mut curr_frame = new_frame();
 
         // Input
@@ -75,6 +78,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 match key_event.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -83,6 +91,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+        //Updates
+        player.update(delta);
 
         // Draw and render section
         player.draw(&mut curr_frame);
