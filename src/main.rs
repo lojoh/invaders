4eef593ec339d92d0029f8ad7aec7cc988ee80c5
Ludwig Lord Johansson <ludwig.johansson@sr.se>
2022-09-27 12:51:08 +0000

    Rendering & multithreading done

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::{event, terminal, ExecutableCommand};
+use invaders::frame::new_frame;
+use invaders::{frame, render};
 use rusty_audio::Audio;
 use std::error::Error;
-use std::io;
+use std::sync::mpsc;
 use std::time::Duration;
+use std::{io, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -27,6 +30,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
+    // TODO: Lopp through audio folder and set this programmtically
     audio.add("explode", "./sounds/explode.wav");
     audio.add("lose", "./sounds/lose.wav");
     audio.add("move", "./sounds/move.wav");
@@ -41,8 +45,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    // TODO: Change this to crossbeam channels instead
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
     // Game Loop
     'gameloop: loop {
+        // Per frame init
+        let curr_frame = new_frame();
+
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -55,9 +79,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Draw and render section
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     // Cleanup
+    drop(render_tx); // Newer versions of Rust doesn't need this. Only here for compatibility
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
