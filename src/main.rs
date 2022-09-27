0a7950cb87bf36375c0edb90e6473442e58573d5
Ludwig Lord Johansson <ludwig.johansson@sr.se>
2022-09-27 12:07:56 +0000

    Basic gameloop

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, terminal, ExecutableCommand};
 use rusty_audio::Audio;
 use std::error::Error;
+use std::io;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
 
     let audio_files_path = std::fs::read_dir("./sounds").unwrap();
 
-    // Can be used to file audio.add
+    //Can be used to file audio.add
     for file in audio_files_path {
-        println!("Name: {}", file.unwrap().path().display());
+        //println!("Name: {}", file.unwrap().path().display());
+        let path = file?.path();
+        // Get path string.
+        let path_str = path.to_str().unwrap();
+        println!("PATH: {}", path_str);
+
+        let split = path_str.split("\\");
+
+        for s in split {
+            println!("s: {}", s);
+        }
     }
 
     audio.add("explode", "./sounds/explode.wav");
@@ -17,9 +33,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("pew", "./sounds/pew.wav");
     audio.add("startup", "./sounds/startup.wav");
     audio.add("win", "./sounds/win.wav");
-    
     audio.play("startup");
+
+    // Terminal setup
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game Loop
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
-
